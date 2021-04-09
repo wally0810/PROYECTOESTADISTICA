ccdc7b091431132101ddcee2bd6560a3c7bcd47a
wally0810 <82188431+wally0810@users.noreply.github.com>
2021-04-08 20:43:33 -0600

    PRIMER FASE

---
+++ PROYECTOESTADISTICA/PROYECTOESTADISTICA/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Ladistribuciónmuestraldeproporciones.Show()
+        Me.Hide()
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        End
+    End Sub
+End Class
